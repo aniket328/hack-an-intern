61ae61166d1d91e11602374b8c2500ed1a63cd25
kowshik27 <kowshikkumar2704@gmail.com>
2022-10-30 21:44:21 +0530

    Added client folders

---
+++ client/pages/test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CardSkeleton from '../components/CardSkeleton'
+
+const Test = () => {
+  return (
+    <div className='p-6'>
+      <CardSkeleton />
+    </div> 
+  )
+}
+
+export default Test
